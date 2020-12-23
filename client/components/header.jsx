ac0831f70ba5c042b69a1aeda5e3470a51fb621c
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-22 17:17:03 -0700

    Navbar progress

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 export default class Header extends React.Component {
   render() {
     return (
-      <header>
-
-      </header>
+    <Navbar expand="md" className="header">
+      <Navbar.Brand>
+        <img
+          src="/cubelogocircle.svg"
+          alt="Logo"
+          className="nav-img"
+        />
+      </Navbar.Brand>
+      <Navbar.Brand>Speed Cube Timer</Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav>
+          <Nav.Link href="#login">Log In/Sign Up</Nav.Link>
+          <Nav.Link href="#timer">Timer</Nav.Link>
+          <Nav.Link href="#world-records">World Records</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
     );
   }
 }
--- client/pages/timer-page.jsx
+++ client/pages/timer-page.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Header from '../components/header';
 
 export default class TimerPage extends React.Component {
   render() {
-    return <button>Hello World!</button>;
+    return (
+    <Header>Hello World!</Header>
+    );
   }
 }
