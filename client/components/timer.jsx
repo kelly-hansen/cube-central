7385104d694e9235fe0131fb744c17166efbcfd9
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-23 11:49:46 -0700

    Timer state setup

--- client/components/timer.jsx
+++ client/components/timer.jsx
@@ -3,11 +3,21 @@ import React from 'react';
 export default class Timer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      running: false,
+      elapsedTime: 0
+    };
   }
 
   render() {
+    let timerClass;
+    if (this.state.running) {
+      timerClass = 'timer timer-running';
+    } else {
+      timerClass = 'timer timer-stopped';
+    }
     const fullTimer = (
-      <div className="timer timer-stopped">
+      <div className={timerClass}>
         <p className="counter">0:00.00</p>
         <p>Start</p>
       </div>
