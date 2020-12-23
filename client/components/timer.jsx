59165c32bd1d74a2138e7f76ce1f11951e8cc6b5
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-23 12:03:42 -0700

    Create start/stop timer methods, set to onClick for component

--- client/components/timer.jsx
+++ client/components/timer.jsx
@@ -7,17 +7,30 @@ export default class Timer extends React.Component {
       running: false,
       elapsedTime: 0
     };
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+  }
+
+  startTimer() {
+
+  }
+
+  stopTimer() {
+
   }
 
   render() {
     let timerClass;
+    let timerMethod;
     if (this.state.running) {
       timerClass = 'timer timer-running';
+      timerMethod = this.stopTimer;
     } else {
       timerClass = 'timer timer-stopped';
+      timerMethod = this.startTimer;
     }
     const fullTimer = (
-      <div className={timerClass}>
+      <div className={timerClass} onClick={timerMethod}>
         <p className="counter">0:00.00</p>
         <p>Start</p>
       </div>
