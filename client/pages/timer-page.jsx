4437abae6ebc49544946e29a2ee3a17c014c7451
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-22 19:00:17 -0700

    Add Virtual Cube button

--- client/pages/timer-page.jsx
+++ client/pages/timer-page.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import Header from '../components/header';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 export default class TimerPage extends React.Component {
   render() {
     return (
-    <Header>Hello World!</Header>
+      <>
+        <Header></Header>
+        <Container>
+          <Row>
+            <Col>
+            </Col>
+            <Col>
+              <Button block>Virtual Cube</Button>
+            </Col>
+          </Row>
+        </Container>
+      </>
     );
   }
 }
