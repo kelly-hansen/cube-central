d52af535fbbb3870fd4e1f28d7acefa6f1f35f5d
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-23 14:20:09 -0700

    Yellow Section creation and styling

--- client/pages/timer-page.jsx
+++ client/pages/timer-page.jsx
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import YellowSection from '../components/yellow-section';
 
 export default class TimerPage extends React.Component {
   render() {
@@ -21,6 +22,7 @@ export default class TimerPage extends React.Component {
             </Col>
           </Row>
         </Container>
+        <YellowSection></YellowSection>
       </>
     );
   }
