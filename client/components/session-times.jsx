58050100e029eefab9a68a1ff9e8f8d7fd6de4b7
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-28 18:08:31 -0800

    deleteTimeByIndex() functionality and pass to onClick of rendered i

--- client/components/session-times.jsx
+++ client/components/session-times.jsx
@@ -8,7 +8,8 @@ export default class SessionTimes extends React.Component {
   }
 
   deleteTimeByIndex(e) {
-
+    const timeIndex = e.target.getAttribute('data-time-index');
+    this.props.deleteTime(parseInt(timeIndex, 10));
   }
 
   render() {
@@ -18,7 +19,11 @@ export default class SessionTimes extends React.Component {
         <div key={`time${i + 1}`} className="d-flex justify-content-center">
           <p className="time-number my-0 text-right">{`${i + 1}.`}</p>
           <p className="time-display my-0 text-center">{displayTime}</p>
-          <i className="fas fa-times-circle time-delete d-flex align-items-center my-0 text-left" data-time-index={i}></i>
+          <i
+            onClick={this.deleteTimeByIndex}
+            className="fas fa-times-circle time-delete d-flex align-items-center my-0 text-left"
+            data-time-index={i}
+          ></i>
         </div>
       );
       return newTime;
