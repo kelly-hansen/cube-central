1bc942537a8c4f41e4d27732c832c0f96ea256fe
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-28 17:56:10 -0800

    Add data-time-index attribute to rendered i

--- client/components/session-times.jsx
+++ client/components/session-times.jsx
@@ -9,7 +9,7 @@ export default class SessionTimes extends React.Component {
         <div key={`time${i + 1}`} className="d-flex justify-content-center">
           <p className="time-number my-0 text-right">{`${i + 1}.`}</p>
           <p className="time-display my-0 text-center">{displayTime}</p>
-          <i className="fas fa-times-circle time-delete d-flex align-items-center my-0 text-left"></i>
+          <i className="fas fa-times-circle time-delete d-flex align-items-center my-0 text-left" data-time-index={i}></i>
         </div>
       );
       return newTime;
