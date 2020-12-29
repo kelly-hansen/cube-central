975c02d39705b260151b4817f30d1041d60c5c85
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-28 16:28:00 -0800

    render statsForRender

--- client/components/session-stats.jsx
+++ client/components/session-stats.jsx
@@ -138,14 +138,7 @@ export default class SessionStats extends React.Component {
 
     return (
       <div className="session-stats">
-        <div className="d-flex justify-content-center mx-3">
-          <p className="stat-col my-0 mr-1 text-right">Best:</p>
-          <p className="stat-col my-0 ml-1 text-left">1:04.43</p>
-        </div>
-        <div className="d-flex justify-content-center mx-3">
-          <p className="stat-col my-0 mr-1 text-right">Worst:</p>
-          <p className="stat-col my-0 ml-1 text-left">1:58.97</p>
-        </div>
+        {statsForRender}
       </div>
     );
   }
