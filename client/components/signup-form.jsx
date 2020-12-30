1f5c278ccbe71b6d444aba99a2d5178cb28831bf
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-29 19:35:46 -0800

    handle change username, password, handle submit methods

--- client/components/signup-form.jsx
+++ client/components/signup-form.jsx
@@ -2,20 +2,58 @@ import React from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 export default class SignUpForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  handleChangePassword(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
   render() {
     return (
       <Container>
         <Row className="d-flex justify-content-center px-3">
           <Col sm="4" className="form-cont p-4 text-center">
             <h5>Sign Up</h5>
-            <Form className="text-center">
+            <Form
+              onSubmit={this.handleSubmit}
+              className="text-center"
+            >
               <Form.Group controlId="username" className="text-left">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="input" placeholder="Enter username" />
+                <Form.Control
+                  type="input"
+                  placeholder="Enter username"
+                  onChange={this.handleChangeUsername}
+                />
               </Form.Group>
               <Form.Group controlId="password" className="text-left">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter password" />
+                <Form.Control
+                  type="password"
+                  placeholder="Enter password"
+                  onChange={this.handleChangePassword}
+                />
                 <Form.Text muted>
                   Password must be at least 8 characters long.
                 </Form.Text>
