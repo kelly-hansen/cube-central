eff6bba5f720ed62c6f42d3efed40d5ac5a9c657
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-29 17:52:14 -0800

    Set up error middleware in index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
+const ClientError = require('./client-error');
+const errorMiddleware = require('./error-middleware');
 
 const app = express();
 
@@ -12,7 +14,15 @@ app.use(jsonMiddleware);
 
 app.post('/api/auth/sign-up', (req, res, next) => {
   const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(400, 'username and password are required fields');
+  }
+  if (password.length < 8) {
+    throw new ClientError(400, 'password must be at least 8 characters');
+  }
 });
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
