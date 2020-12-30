f6ad5f45f2d5adfe0579640b0a68dc457ec163b2
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-30 15:23:58 -0800

    Home logo/title layout

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default class Home extends React.Component {
   render() {
     return (
-      <></>
+      <Container>
+        <Row>
+          <Col className="d-flex justify-content-center align-items-center my-4">
+            <img
+              src="/cubelogocircle.svg"
+              alt="Logo"
+              className="home-img mr-3"
+            />
+            <div>
+              <h3 className="title-text mb-0">Speed Cube</h3>
+              <h3 className="title-text mb-0">Timer</h3>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
