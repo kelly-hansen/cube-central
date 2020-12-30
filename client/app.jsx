05bf24f64ed9a9ea96150214993cb0805dd6d63a
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-29 16:06:05 -0800

    App constructor with route property in state

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import TimerPage from './pages/timer-page';
+import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
   render() {
     return <TimerPage />;
   }
