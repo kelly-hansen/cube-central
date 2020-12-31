087cf6f4ef32321e07fe4c897c3ed665dbc2e99f
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-30 17:03:27 -0800

    Profile page layout

--- client/pages/profile.jsx
+++ client/pages/profile.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import Header from '../components/header';
+import YellowSection from '../components/yellow-section';
 
 export default class Profile extends React.Component {
   render() {
     return (
-      <></>
+      <>
+        <Header />
+        <div className="d-flex justify-content-center align-items-center my-4">
+          <img
+            src="/cubelogoiconbw.svg"
+            alt="Cube Icon"
+            className="profile-cube-icon mr-3"
+          />
+          <h5 className="mb-0">Username</h5>
+        </div>
+        <div className="d-flex flex-column align-items-center">
+          <p className="mb-1">Member Since:</p>
+          <p>joinDate</p>
+        </div>
+        <YellowSection></YellowSection>
+      </>
     );
   }
 }
