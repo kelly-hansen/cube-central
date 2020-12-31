ef28f9104799feefde53898896b3b2f824a0071b
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-30 18:57:06 -0800

    Display username and joinDate on profile page via context

--- client/pages/profile.jsx
+++ client/pages/profile.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Header from '../components/header';
 import YellowSection from '../components/yellow-section';
+import AppContext from '../lib/app-context';
 
 export default class Profile extends React.Component {
   render() {
+    const joinDate = new Date(this.context.user.joinDate);
+
     return (
       <>
         <Header />
@@ -13,14 +16,16 @@ export default class Profile extends React.Component {
             alt="Cube Icon"
             className="profile-cube-icon mr-3"
           />
-          <h5 className="mb-0">Username</h5>
+          <h5 className="mb-0">{this.context.user.username}</h5>
         </div>
         <div className="d-flex flex-column align-items-center">
           <p className="mb-1">Member Since:</p>
-          <p>joinDate</p>
+          <p>{joinDate.toLocaleDateString()}</p>
         </div>
         <YellowSection></YellowSection>
       </>
     );
   }
 }
+
+Profile.contextType = AppContext;
