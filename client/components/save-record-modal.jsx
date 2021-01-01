a9e034e55d92ba57de06b1d7841295d326b2221c
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-31 17:22:47 -0800

    SaveRecordModal layout complete

--- client/components/save-record-modal.jsx
+++ client/components/save-record-modal.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Modal, Form } from 'react-bootstrap';
+import { Modal, Form, Button } from 'react-bootstrap';
 
 export default class SaveRecordModal extends React.Component {
   render() {
     return (
       <Modal show={true}>
         <Modal.Header>
-          <h3>Save Record</h3>
+          <h5>Save New Record</h5>
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group>
+            <Form.Group controlId="puzzle">
               <Form.Label>Puzzle</Form.Label>
               <Form.Control as="select">
                 <option>3x3x3 Cube</option>
@@ -25,6 +25,17 @@ export default class SaveRecordModal extends React.Component {
                 <option>Square-1</option>
               </Form.Control>
             </Form.Group>
+            <Form.Group controlId="record-type">
+              <Form.Label>Record Type</Form.Label>
+              <Form.Control as="select">
+                <option>Single</option>
+                <option>Average 3 of 5</option>
+              </Form.Control>
+            </Form.Group>
+            <div className="d-flex justify-content-end">
+              <Button variant="secondary" className="align-self-end">Cancel</Button>
+              <Button type="submit" className="ml-2">Submit New Record</Button>
+            </div>
           </Form>
         </Modal.Body>
       </Modal>
