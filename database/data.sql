74981ec165659cd3f9899cab032bbd9ad3eae2ad
Kelly Hansen <kellyhansen182@gmail.com>
2020-12-31 17:34:26 -0800

    Update db with puzzleTypes and recordTypes

---
+++ database/data.sql
@@ -0,0 +1,15 @@
+insert into "puzzleTypes" ("label")
+     values ('3x3x3 Cube'),
+            ('2x2x2 Cube'),
+            ('4x4x4 Cube'),
+            ('5x5x5 Cube'),
+            ('3x3x3 One-Handed'),
+            ('Clock'),
+            ('Megminx'),
+            ('Pyraminx'),
+            ('Skewb'),
+            ('Square-1');
+
+insert into "recordTypes" ("label")
+     values ('Single'),
+            ('Average 3 of 5');
