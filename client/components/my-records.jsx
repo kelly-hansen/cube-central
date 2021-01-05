d331aec83f0f0584571eb3cb7911b6a1bce15ab9
Kelly Hansen <kellyhansen182@gmail.com>
2021-01-04 18:06:36 -0800

    Initialize MyRecords state

--- client/components/my-records.jsx
+++ client/components/my-records.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 export default class MyRecords extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      puzzletype: '3x3x3 Cube',
+      records: {
+        bestSingle: null,
+        bestAverage3Of5Arr: null
+      }
+    };
+  }
+
   render() {
     return (
       <>
