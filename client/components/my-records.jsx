f0e82d7643638ed209ec2d7e509c29ee660840d0
Kelly Hansen <kellyhansen182@gmail.com>
2021-01-04 19:08:31 -0800

    MyRecords layout progress

--- client/components/my-records.jsx
+++ client/components/my-records.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row, Col, Form } from 'react-bootstrap';
 
 export default class MyRecords extends React.Component {
   constructor(props) {
@@ -7,15 +8,67 @@ export default class MyRecords extends React.Component {
       puzzletype: '3x3x3 Cube',
       records: {
         bestSingle: null,
-        bestAverage3Of5Arr: null
+        bestAverage3Of5Arr: 7
       }
     };
   }
 
   render() {
+    const averageTimes = this.state.records.bestAverage3Of5Arr
+      ? (
+      <Row>
+        <Col className="d-flex justify-content-center">
+          <Col className="mx-2">0:35.53</Col>
+          <Col className="mx-2">0:35.53</Col>
+          <Col className="mx-2">0:35.53</Col>
+          <Col className="mx-2">0:35.53</Col>
+          <Col className="mx-2">0:35.53</Col>
+        </Col>
+      </Row>
+        )
+      : null;
+
     return (
-      <>
-      </>
+      <Container className="mb-4">
+        <Row>
+          <Col className="mb-2">
+            <p>My Records</p>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col className="puzzle-selector  mb-2">
+            <Form>
+              <Form.Group controlId="puzzleType">
+                <Form.Control as="select">
+                  <option>3x3x3 Cube</option>
+                  <option>2x2x2 Cube</option>
+                  <option>4x4x4 Cube</option>
+                  <option>5x5x5 Cube</option>
+                  <option>3x3x3 One-Handed</option>
+                  <option>Clock</option>
+                  <option>Megaminx</option>
+                  <option>Pyraminx</option>
+                  <option>Skewb</option>
+                  <option>Square-1</option>
+                </Form.Control>
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="d-flex justify-content-center mb-3">
+            <p className="stat-col my-0 mr-1 text-right">Single:</p>
+            <p className="stat-col my-0 ml-1 text-left">N/A</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col md className="d-flex justify-content-center mb-3">
+            <p className="stat-col my-0 mr-1 text-right">Best Avg 3 Of 5:</p>
+            <p className="stat-col my-0 ml-1 text-left">N/A</p>
+          </Col>
+        </Row>
+        {averageTimes}
+      </Container>
     );
   }
 }
--- client/lib/get-display-time.js
+++ client/lib/get-display-time.js
@@ -5,11 +5,9 @@ export default function getDisplayTime(timeInput) {
   const sec = Math.floor(time / 1000).toString(10);
   time = time - (sec * 1000);
   const hundreths = Math.floor(time / 10).toString(10);
-  let displayedSec;
-  sec.length === 1 ? displayedSec = '0' + sec : displayedSec = sec;
   let displayedHundreths;
   hundreths.length === 1 ? displayedHundreths = '0' + hundreths : displayedHundreths = hundreths;
-  const displayedTime = `${min}:${displayedSec}.${displayedHundreths}`;
+  const displayedTime = `${min !== 0 ? min + ':' : ''}${sec}.${displayedHundreths}`;
 
   return displayedTime;
 }
