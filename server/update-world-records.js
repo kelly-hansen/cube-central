9ae6550f35a99d244d9440d011a16d7a08ad2bf7
Kelly Hansen <kellyhansen182@gmail.com>
2021-01-05 17:39:46 -0800

    Web scraping progress

--- server/update-world-records.js
+++ server/update-world-records.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 
+/*
 fetch('https://www.worldcubeassociation.org/results/records')
   .then(res => res.text())
   .then(body => {
@@ -9,3 +10,18 @@ fetch('https://www.worldcubeassociation.org/results/records')
     const trimmedPuzzle = puzzle.trim();
     console.log(trimmedPuzzle);
   });
+*/
+
+const fs = require('fs');
+
+let wcaData;
+fs.readFile('./wcadata.html', 'utf8', (err, data) => {
+  if (err) throw err;
+  wcaData = data;
+  const $ = cheerio.load(wcaData);
+  const puzzleTypes = $('h2 > a', '#results-list');
+  const puzzleTypesArr = puzzleTypes.map((i, el) => {
+    return $(el).text().trim();
+  }).get();
+  console.log(puzzleTypesArr);
+});
