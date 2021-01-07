fd176ed163fa701ce74799af87295a80a30585f5
Kelly Hansen <kellyhansen182@gmail.com>
2021-01-06 16:07:03 -0800

    Use fetch with update-world-records module

--- server/update-world-records.js
+++ server/update-world-records.js
@@ -7,61 +7,52 @@ const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL
 });
 
-/*
 fetch('https://www.worldcubeassociation.org/results/records')
   .then(res => res.text())
   .then(body => {
     const $ = cheerio.load(body);
-  });
-*/
 
-const fs = require('fs');
+    const puzzleTypes = $('h2 > a', '#results-list');
+    const puzzles = puzzleTypes.map((i, el) => {
+      return $(el).text().trim();
+    }).get();
 
-fs.readFile('./server/wcadata.html', 'utf8', (err, data) => {
-  if (err) throw err;
-  const $ = cheerio.load(data);
-
-  const puzzleTypes = $('h2 > a', '#results-list');
-  const puzzles = puzzleTypes.map((i, el) => {
-    return $(el).text().trim();
-  }).get();
-
-  const tableBodys = $('tbody', '#results-list');
-  const rows = tableBodys.map((i, el) => {
-    return [$(el).children('tr').map((i, el) => {
-      return [$(el).children('td').map((i, el) => {
-        return $(el).text().trim();
+    const tableBodys = $('tbody', '#results-list');
+    const rows = tableBodys.map((i, el) => {
+      return [$(el).children('tr').map((i, el) => {
+        return [$(el).children('td').map((i, el) => {
+          return $(el).text().trim();
+        }).get()];
       }).get()];
-    }).get()];
-  }).get();
+    }).get();
 
-  const records = [];
-  for (let i = 0; i < puzzles.length; i++) {
+    const records = [];
+    for (let i = 0; i < puzzles.length; i++) {
 
-    records.push({
-      puzzle: puzzles[i],
-      rows: []
-    });
-
-    for (let j = 0; j < rows[i].length; j++) {
-      records[i].rows.push({
-        type: rows[i][j][0],
-        name: rows[i][j][1],
-        result: rows[i][j][2]
+      records.push({
+        puzzle: puzzles[i],
+        rows: []
       });
-    }
-  }
 
-  const recordsJson = JSON.stringify(records);
-  const dateUpdated = 'now()';
-  const sql = `
-    with "deleted" as (
-      delete from "worldRecordsData" *
-    )
-    insert into "worldRecordsData" ("recordsData", "dateUpdated")
-    values ($1, $2);
-    `;
-  const params = [recordsJson, dateUpdated];
-  db.query(sql, params)
-    .catch(err => console.error(err));
-});
+      for (let j = 0; j < rows[i].length; j++) {
+        records[i].rows.push({
+          type: rows[i][j][0],
+          name: rows[i][j][1],
+          result: rows[i][j][2]
+        });
+      }
+    }
+
+    const recordsJson = JSON.stringify(records);
+    const dateUpdated = 'now()';
+    const sql = `
+      with "deleted" as (
+        delete from "worldRecordsData" *
+      )
+      insert into "worldRecordsData" ("recordsData", "dateUpdated")
+      values ($1, $2);
+      `;
+    const params = [recordsJson, dateUpdated];
+    return db.query(sql, params);
+  })
+  .catch(err => console.error(err));
