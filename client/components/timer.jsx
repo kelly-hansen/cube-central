cf7d78a2fa25346ad168411a9f44d5da64d1a380
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-03 13:28:40 -0800

    Timer convert to hooks, useRef for event listeners

--- client/components/timer.jsx
+++ client/components/timer.jsx
@@ -1,63 +1,76 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import getDisplayTime from '../lib/get-display-time';
 
 export default function Timer(props) {
 
-  const [running, setRunning] = useState(false);
-  const [elapsed, setElapsed] = useState(0);
-  const [intervalId, setIntervalId] = useState(null);
+  const [running, _setRunning] = useState(false);
+  const runningRef = useRef(running);
+  function setRunning(status) {
+    runningRef.current = status;
+    _setRunning(status);
+  }
+
+  const [elapsed, _setElapsed] = useState(0);
+  const elapsedRef = useRef(elapsed);
+  function setElapsed(number) {
+    elapsedRef.current = number;
+    _setElapsed(number);
+  }
+
+  const [intervalId, _setIntervalId] = useState(null);
+  const intervalIdRef = useRef(intervalId);
+  function setIntervalId(id) {
+    intervalIdRef.current = id;
+    _setIntervalId(id);
+  }
 
   function startTimer() {
     setRunning(true);
     setElapsed(0);
-    const intervalId = setInterval(() => {
-      setElapsed(elapsed + 10);
-      setIntervalId(intervalId);
+    const newIntervalId = setInterval(() => {
+      _setElapsed(prevElapsed => {
+        elapsedRef.current = prevElapsed + 10;
+        return prevElapsed + 10;
+      });
+      setIntervalId(newIntervalId);
     }, 10);
   }
 
   function stopTimer() {
-    clearInterval(this.state.intervalId);
-    const elapsed = this.state.elapsed;
-    this.props.addNewTime(elapsed);
-    this.setState({
-      running: false,
-      elapsed
-    });
+    clearInterval(intervalIdRef.current);
+    props.addNewTime(elapsedRef.current);
+    setRunning(false);
   }
 
-  spaceListener(e) {
+  function spaceListener(e) {
     if (e.code === 'Space') {
-      !this.state.running ? this.startTimer() : this.stopTimer();
+      !runningRef.current ? startTimer() : stopTimer();
       if (e.target === document.body) {
         e.preventDefault();
       }
     }
   }
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.spaceListener);
-  }
+  useEffect(() => {
+    window.addEventListener('keydown', spaceListener);
+    return () => {
+      window.removeEventListener('keydown', spaceListener);
+    };
+  }, []);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.spaceListener);
-  }
+  const displayedTime = getDisplayTime(elapsed);
+  let timerStatusClass;
+  running ? timerStatusClass = 'timer-running' : timerStatusClass = 'timer-stopped';
 
-  render() {
-    const displayedTime = getDisplayTime(this.state.elapsed);
-    let timerStatusClass;
-    this.state.running ? timerStatusClass = 'timer-running' : timerStatusClass = 'timer-stopped';
+  const fullTimer = (
+    <div
+    className={`timer ${timerStatusClass} d-flex flex-column justify-content-center align-items-center`}
+    onClick={running ? stopTimer : startTimer}
+    >
+      <p className="counter mb-0">{displayedTime}</p>
+      <p className="start-stop">{running ? 'STOP' : 'START'}</p>
+    </div>
+  );
 
-    const fullTimer = (
-      <div
-      className={`timer ${timerStatusClass} d-flex flex-column justify-content-center align-items-center`}
-      onClick={this.state.running ? this.stopTimer : this.startTimer}
-      >
-        <p className="counter mb-0">{displayedTime}</p>
-        <p className="start-stop">{this.state.running ? 'STOP' : 'START'}</p>
-      </div>
-    );
-
-    return fullTimer;
-  }
+  return fullTimer;
 }
