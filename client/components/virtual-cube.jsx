3f57471ce3b2c3c9447718f21f76bff5b684fdeb
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-04 10:11:32 -0800

    VirtualCube convert to hooks

--- client/components/virtual-cube.jsx
+++ client/components/virtual-cube.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import virtualCube from '../lib/virtualcube';
 
-export default class VirtualCube extends React.Component {
-
-  render() {
-    virtualCube();
-    return <div className="virtualcube" kind={this.props.type} stickersimage="cubecolors.gif"></div>;
-  }
+export default function VirtualCube(props) {
+  virtualCube();
+  return <div className="virtualcube" kind={props.type} stickersimage="cubecolors.gif"></div>;
 }
