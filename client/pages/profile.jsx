231317d4dec95574583cc62df733684c44a78c27
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-04 10:23:00 -0800

    Profile convert to hooks

--- client/pages/profile.jsx
+++ client/pages/profile.jsx
@@ -1,34 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/header';
 import YellowSection from '../components/yellow-section';
 import MyRecords from '../components/my-records';
 import AppContext from '../lib/app-context';
 
-export default class Profile extends React.Component {
-  render() {
-    const joinDate = new Date(this.context.user.joinDate);
+export default function Profile() {
+  const context = useContext(AppContext);
 
-    return (
-      <>
-        <Header />
-        <div className="d-flex justify-content-center align-items-center my-4">
-          <img
-            src="/cubelogoiconbw.svg"
-            alt="Cube Icon"
-            className="profile-cube-icon mr-3"
-          />
-          <h5 className="mb-0">{this.context.user.username}</h5>
-        </div>
-        <div className="d-flex flex-column align-items-center">
-          <p className="mb-1">Member Since:</p>
-          <p>{joinDate.toLocaleDateString()}</p>
-        </div>
-        <YellowSection>
-          <MyRecords />
-        </YellowSection>
-      </>
-    );
-  }
+  const joinDate = new Date(context.user.joinDate);
+
+  return (
+    <>
+      <Header />
+      <div className="d-flex justify-content-center align-items-center my-4">
+        <img
+          src="/cubelogoiconbw.svg"
+          alt="Cube Icon"
+          className="profile-cube-icon mr-3"
+        />
+        <h5 className="mb-0">{context.user.username}</h5>
+      </div>
+      <div className="d-flex flex-column align-items-center">
+        <p className="mb-1">Member Since:</p>
+        <p>{joinDate.toLocaleDateString()}</p>
+      </div>
+      <YellowSection>
+        <MyRecords />
+      </YellowSection>
+    </>
+  );
 }
-
-Profile.contextType = AppContext;
