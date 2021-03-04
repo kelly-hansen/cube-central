cc94c898d1c2de4fb07e67bdfd17c751ef2afb64
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-04 11:00:47 -0800

    TimerPage convert too hooks

--- client/pages/timer-page.jsx
+++ client/pages/timer-page.jsx
@@ -10,28 +10,6 @@ import AppContext from '../lib/app-context';
 import VirtualCube from '../components/virtual-cube';
 
 export default function TimerPage() {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sessionTimes: [],
-      sessionRecords: null,
-      showResetModal: false,
-      showSaveRecordModal: false,
-      virtualCube: {
-        active: false,
-        type: 'RubiksCube'
-      }
-    };
-    this.getSessionRecords = this.getSessionRecords.bind(this);
-    this.addNewTime = this.addNewTime.bind(this);
-    this.deleteTime = this.deleteTime.bind(this);
-    this.resetSession = this.resetSession.bind(this);
-    this.toggleResetModal = this.toggleResetModal.bind(this);
-    this.toggleSaveRecordModal = this.toggleSaveRecordModal.bind(this);
-    this.toggleVirtualCube = this.toggleVirtualCube.bind(this);
-    this.changeCubeType = this.changeCubeType.bind(this);
-  }
-
   const [sessionTimes, setSessionTimes] = useState([]);
   const [sessionRecords, setSessionRecords] = useState(null);
   const [showResetModal, setShowResetModal] = useState(false);
@@ -43,22 +21,22 @@ export default function TimerPage() {
 
   const context = useContext(AppContext);
 
-  getSessionRecords(sessionTimes) {
-    if (sessionTimes.length === 0) {
+  function getSessionRecords(sesTimes) {
+    if (sesTimes.length === 0) {
       return null;
     }
 
     const result = {};
-    result.bestSingle = [sessionTimes.length > 0 ? Math.min(...sessionTimes) : null];
+    result.bestSingle = [sesTimes.length > 0 ? Math.min(...sesTimes) : null];
 
-    if (sessionTimes.length < 5) {
+    if (sesTimes.length < 5) {
       result.bestAverage3Of5 = null;
       result.bestAverage3Of5Arr = null;
     } else {
       let bestAvg;
       let bestStartingIndex = 0;
-      for (let i = 0; i < sessionTimes.length - 4; i++) {
-        const sortedSet = sessionTimes.slice(i, i + 5).sort((a, b) => a - b);
+      for (let i = 0; i < sesTimes.length - 4; i++) {
+        const sortedSet = sesTimes.slice(i, i + 5).sort((a, b) => a - b);
         const setOf3 = sortedSet.slice(1, 4);
         const avg = setOf3.reduce((acc, cur) => acc + cur) / 3;
         if (bestAvg) {
@@ -71,152 +49,137 @@ export default function TimerPage() {
         }
       }
       result.bestAverage3Of5 = [bestAvg];
-      result.bestAverage3Of5Arr = sessionTimes.slice(bestStartingIndex, bestStartingIndex + 5);
+      result.bestAverage3Of5Arr = sesTimes.slice(bestStartingIndex, bestStartingIndex + 5);
     }
 
     return result;
   }
 
-  addNewTime(time) {
-    const sessionTimes = this.state.sessionTimes.concat(time);
-    const sessionRecords = this.getSessionRecords(sessionTimes);
-    this.setState({
-      sessionTimes,
-      sessionRecords
+  function addNewTime(time) {
+    const sesTimes = sessionTimes.concat(time);
+    const sesRecords = getSessionRecords(sesTimes);
+    setSessionTimes(sesTimes);
+    setSessionRecords(sesRecords);
+  }
+
+  function deleteTime(index) {
+    const sesTimes = sessionTimes.slice();
+    sesTimes.splice(index, 1);
+    const sesRecords = getSessionRecords(sesTimes);
+    setSessionTimes(sesTimes);
+    setSessionRecords(sesRecords);
+  }
+
+  function resetSession() {
+    setSessionTimes([]);
+    setSessionRecords(null);
+    setShowResetModal(false);
+  }
+
+  function toggleResetModal() {
+    setShowResetModal(prev => !prev);
+  }
+
+  function toggleSaveRecordModal() {
+    setShowSaveRecordModal(prev => !prev);
+  }
+
+  function toggleVirtualCube() {
+    setVirtualCube(prev => {
+      return {
+        active: !prev.active,
+        type: prev.type
+      };
     });
   }
 
-  deleteTime(index) {
-    const sessionTimes = this.state.sessionTimes.slice();
-    sessionTimes.splice(index, 1);
-    const sessionRecords = this.getSessionRecords(sessionTimes);
-    this.setState({
-      sessionTimes,
-      sessionRecords
-    });
-  }
-
-  resetSession() {
-    this.setState({
-      sessionTimes: [],
-      sessionRecords: null,
-      showResetModal: false
-    });
-  }
-
-  toggleResetModal() {
-    this.setState({
-      showResetModal: !this.state.showResetModal
-    });
-  }
-
-  toggleSaveRecordModal() {
-    this.setState({
-      showSaveRecordModal: !this.state.showSaveRecordModal
-    });
-  }
-
-  toggleVirtualCube() {
-    this.setState({
-      virtualCube: {
-        active: !this.state.virtualCube.active,
-        type: this.state.virtualCube.type
-      }
-    });
-  }
-
-  changeCubeType() {
-    this.setState({
-      virtualCube: {
-        active: this.state.virtualCube.active,
-        type: this.state.virtualCube.type === 'RubiksCube'
+  function changeCubeType() {
+    setVirtualCube(prev => {
+      return {
+        active: prev.active,
+        type: prev.type === 'RubiksCube'
           ? 'PocketCube'
           : 'RubiksCube'
-      }
+      };
     });
   }
 
-  render() {
-
-    return (
-      <>
-        <Header />
-        <Container>
-          <Row className={this.state.virtualCube.active ? 'justify-content-center mb-2' : 'justify-content-center mb-3'}>
-            {
-              !this.state.virtualCube.active
-                ? (
-                <Col md={8} lg={6} xl={6}>
-                  <Button onClick={this.toggleVirtualCube} className="std-button" block>Virtual Cube</Button>
-                </Col>
-                  )
-                : (
-                <>
-                  <Col md={8} lg={6} xl={6} className="d-flex">
-                    <div className="w-50 pr-1">
-                      <Button onClick={this.toggleVirtualCube} className="std-button" block>Hide Cube</Button>
-                    </div>
-                    <div className="w-50 pl-1">
-                      <Button onClick={this.changeCubeType} className="std-button" block>Change Type</Button>
-                    </div>
-                  </Col>
-                </>
-                  )
-            }
-          </Row>
-          {this.state.virtualCube.active && (
-            <Row className="justify-content-center mb-4">
+  return (
+    <>
+      <Header />
+      <Container>
+        <Row className={virtualCube.active ? 'justify-content-center mb-2' : 'justify-content-center mb-3'}>
+          {
+            !virtualCube.active
+              ? (
               <Col md={8} lg={6} xl={6}>
-                <VirtualCube type={this.state.virtualCube.type} />
+                <Button onClick={toggleVirtualCube} className="std-button" block>Virtual Cube</Button>
               </Col>
-            </Row>
-          )}
-          <Row className="justify-content-center">
+                )
+              : (
+              <>
+                <Col md={8} lg={6} xl={6} className="d-flex">
+                  <div className="w-50 pr-1">
+                    <Button onClick={toggleVirtualCube} className="std-button" block>Hide Cube</Button>
+                  </div>
+                  <div className="w-50 pl-1">
+                    <Button onClick={changeCubeType} className="std-button" block>Change Type</Button>
+                  </div>
+                </Col>
+              </>
+                )
+          }
+        </Row>
+        {virtualCube.active && (
+          <Row className="justify-content-center mb-4">
             <Col md={8} lg={6} xl={6}>
-              <Timer addNewTime={this.addNewTime} />
+              <VirtualCube type={virtualCube.type} />
+            </Col>
+          </Row>
+        )}
+        <Row className="justify-content-center">
+          <Col md={8} lg={6} xl={6}>
+            <Timer addNewTime={addNewTime} />
+          </Col>
+        </Row>
+      </Container>
+      <YellowSection>
+        <Container>
+          <Row>
+            <Col>
+              <p>Session</p>
+            </Col>
+          </Row>
+          <Row className="mb-4">
+            <Col sm className="d-flex justify-content-center mb-4">
+              <SessionStats sessionTimes={sessionTimes} />
+            </Col>
+            <Col sm className="d-flex justify-content-center">
+              <SessionTimes sessionTimes={sessionTimes} deleteTime={deleteTime} />
+            </Col>
+          </Row>
+          <Row className="justify-content-center">
+            {context.user && <Col md={8} lg className="mt-3">
+              <Button onClick={toggleSaveRecordModal} className="std-button" block>Save Record</Button>
+            </Col>}
+            <Col md={8} lg className="mt-3">
+              <Button onClick={toggleResetModal} className="std-button" variant="danger" block>Reset Session</Button>
             </Col>
           </Row>
         </Container>
-        <YellowSection>
-          <Container>
-            <Row>
-              <Col>
-                <p>Session</p>
-              </Col>
-            </Row>
-            <Row className="mb-4">
-              <Col sm className="d-flex justify-content-center mb-4">
-                <SessionStats sessionTimes={this.state.sessionTimes} />
-              </Col>
-              <Col sm className="d-flex justify-content-center">
-                <SessionTimes sessionTimes={this.state.sessionTimes} deleteTime={this.deleteTime} />
-              </Col>
-            </Row>
-            <Row className="justify-content-center">
-              {this.context.user && <Col md={8} lg className="mt-3">
-                <Button onClick={this.toggleSaveRecordModal} className="std-button" block>Save Record</Button>
-              </Col>}
-              <Col md={8} lg className="mt-3">
-                <Button onClick={this.toggleResetModal} className="std-button" variant="danger" block>Reset Session</Button>
-              </Col>
-            </Row>
-          </Container>
-        </YellowSection>
-        <Modal show={this.state.showResetModal} onHide={this.toggleResetModal}>
-          <Modal.Body>Are you sure you want to reset the session? All current times will be lost.</Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.toggleResetModal} variant="secondary">Cancel</Button>
-            <Button onClick={this.resetSession} variant="primary">Reset Session</Button>
-          </Modal.Footer>
-        </Modal>
-        <SaveRecordModal
-          sessionRecords={this.state.sessionRecords}
-          toggleSaveRecordModal={this.toggleSaveRecordModal}
-          showModal={this.state.showSaveRecordModal}
-        />
-      </>
-    );
-  }
+      </YellowSection>
+      <Modal show={showResetModal} onHide={toggleResetModal}>
+        <Modal.Body>Are you sure you want to reset the session? All current times will be lost.</Modal.Body>
+        <Modal.Footer>
+          <Button onClick={toggleResetModal} variant="secondary">Cancel</Button>
+          <Button onClick={resetSession} variant="primary">Reset Session</Button>
+        </Modal.Footer>
+      </Modal>
+      <SaveRecordModal
+        sessionRecords={sessionRecords}
+        toggleSaveRecordModal={toggleSaveRecordModal}
+        showModal={showSaveRecordModal}
+      />
+    </>
+  );
 }
-
-TimerPage.contextType = AppContext;
