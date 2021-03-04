3fa678f6d26f1e49f5aad4684c09e9fae12ba496
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-04 10:19:16 -0800

    LogInSignUpPage convert to hooks

--- client/pages/log-in-sign-up-page.jsx
+++ client/pages/log-in-sign-up-page.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/header';
 import SignUpForm from '../components/sign-up-form';
 import LogInForm from '../components/log-in-form';
 import AppContext from '../lib/app-context';
 
-export default class LogInSignUpPage extends React.Component {
-  render() {
-    return (
-      <>
-        <Header />
-        {this.context.route.path === 'log-in' ? <LogInForm /> : <SignUpForm />}
-      </>
-    );
-  }
+export default function LogInSignUpPage() {
+  const context = useContext(AppContext);
+  return (
+    <>
+      <Header />
+      {context.route.path === 'log-in' ? <LogInForm /> : <SignUpForm />}
+    </>
+  );
 }
-
-LogInSignUpPage.contextType = AppContext;
