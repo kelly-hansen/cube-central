a3f46f8534f9c0ccc828511ac56bdbd238def440
Kelly Hansen <kellyhansen182@gmail.com>
2021-04-13 12:12:10 -0700

    Convert Navbar to router Links

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,6 +3,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import AppContext from '../lib/app-context';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   const { user, handleLogOut } = useContext(AppContext);
@@ -10,22 +11,22 @@ export default function Header() {
 
   return (
   <Navbar expand="md" className="header mb-5" collapseOnSelect onSelect={goHome}>
-    <Navbar.Brand href="#">
+    <Navbar.Brand as={Link} to="/">
       <img
         src="/cubelogocircle.svg"
         alt="Logo"
         className="nav-img"
       />
     </Navbar.Brand>
-    <Navbar.Brand href="#">Cube Central</Navbar.Brand>
+    <Navbar.Brand as={Link} to="/">Cube Central</Navbar.Brand>
     <Navbar.Toggle />
     <Navbar.Collapse className="justify-content-end">
       <Nav>
         {user && <Nav.Link className="text-center" disabled>{`Welcome, ${user.username}!`}</Nav.Link>}
-        {user && <Nav.Link className="text-center" href="#">Profile</Nav.Link>}
-        <Nav.Link className="text-center" href="#timer">Timer</Nav.Link>
-        <Nav.Link className="text-center" href="#world-records">World Records</Nav.Link>
-        {user === null ? <Button href="#log-in">Log In/Sign Up</Button> : <Button onClick={handleLogOut}>Log Out</Button>}
+        {user && <Nav.Link as={Link} className="text-center" to="/">Profile</Nav.Link>}
+        <Nav.Link as={Link} className="text-center" to="/timer">Timer</Nav.Link>
+        <Nav.Link as={Link} className="text-center" to="/world-records">World Records</Nav.Link>
+        {user === null ? <Button as={Link} to="/log-in">Log In/Sign Up</Button> : <Button onClick={handleLogOut}>Log Out</Button>}
       </Nav>
     </Navbar.Collapse>
   </Navbar>
